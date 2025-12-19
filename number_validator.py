c2232c1df76ea618a2250553ab08438918f5d17e
Prateek-Shar <147049043+Prateek-Shar@users.noreply.github.com>
2025-12-19 09:05:54 +0530

    latest codes

--- number_validator.py
+++ number_validator.py
@@ -1,6 +1,5 @@
 import re
 
 def validate_number(number):
-    real_number = str(number)
     pattern = r'^[6-9]\d{9}$'
-    return bool(re.match(pattern, real_number))
+    return bool(re.match(pattern, number))
--- validator_main.py
+++ validator_main.py
@@ -1,8 +1,8 @@
 from number_validator import validate_number 
 from email_validator  import validate_gmail
 
-email = "abc@gmail.com"
-contact = 9278197524
+email = input("Enter the email : ")
+contact = (input("Enter the number : "))
 
 print(validate_number(contact))
 print(validate_gmail(email))
